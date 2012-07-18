94cc8d35d3823c7a2b1bd2c555a39b544115587e
Alok Chaturvedi <alok.chauve@gmail.com>
2012-07-18 15:11:49 +0530

    no message

---
+++ SpringerLink/SpringerLink/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  SpringerLink
+//
+//  Created by RAVI DAS on 18/07/12.
+//  Copyright (c) 2012 kiwi technologies india pvt ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test : UIViewController
+
+@end
---
+++ SpringerLink/SpringerLink/test.m
@@ -0,0 +1,43 @@
+//
+//  test.m
+//  SpringerLink
+//
+//  Created by RAVI DAS on 18/07/12.
+//  Copyright (c) 2012 kiwi technologies india pvt ltd. All rights reserved.
+//
+
+#import "test.h"
+
+@interface test ()
+
+@end
+
+@implementation test
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return YES;
+}
+
+@end
